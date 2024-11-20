33aa12e6bcaa16cd2bb74eec9b4ca6cf2e44c3ea
gguerini129 <gguerini999@gmail.com>
2024-11-20 03:57:15 -0500

    Rewrote the demo using modern practice

---
+++ README.md
@@ -0,0 +1,5 @@
+Calculates TFIDF scores for documents that a term appears in.
+The path of the documents at which to search can be supplied by the user.
+Terms that are ubiquitious generally score lower than terms that are more unique.
+A document with a standout relevancy to a term, meaning many mentions, can score high.
+TFIDF algorithms are an effective method to judge the relevancy of documents.
--- src/Main.java
+++ src/Main.java
@@ -1,177 +1,161 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main
 {
-    public static void main(
-        final String[] args)
+    public static final int DOCUMENTS_TO_SHOW = 10;
+
+    public static void main(final String[] args)
     {
-        final Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Enter the relative file directory path: ");
-        final String folderPath = sc.nextLine();
-        
-        final Map<String, List<File>> invertedIndex = createInvertedIndex(folderPath);
-        
-        String query = "placeholder";
-        
-        while(!query.equals(""))
+        try(final Scanner inputScanner = new Scanner(System.in))
         {
-            System.out.print("Enter the term query: ");
-            query = sc.nextLine();
-            
-            System.out.println("Printing the 5 documents most relevant to the query...");
-            System.out.println();
-            
-            List<File> documents = invertedIndex.get(query);
-            documents = documents == null ? new ArrayList<>() : documents;
-            
-            final double[] tfidfs = new double[documents.size()];
-            final int totalNumberOfDocuments = new File(folderPath).listFiles().length;
-            final double idf = documents.size() > 0 ? Math.log(totalNumberOfDocuments / documents.size()) : 0;
-            
-            for(int i = 0; i < documents.size(); i++)
+            System.out.print("Enter the path to where the documents are: ");
+            final Path directory = Paths.get(inputScanner.nextLine());
+
+            final Map<String, Set<Path>> invertedIndex = createInvertedIndex(directory);
+
+            final long totalDocuments = Files.list(directory).count();
+
+            while(true)
             {
-                int totalTerms = 0;
-                
-                try(BufferedReader br = new BufferedReader(new FileReader(documents.get(i))))
+                System.out.print("Enter the query term: ");
+                final String queryTerm = inputScanner.nextLine();
+
+                if(queryTerm.equals(""))
                 {
-                    String line = br.readLine();
-                    
-                    while(line != null)
-                    {
-                        line = line.toLowerCase().replaceAll("[^a-z]+", " ").trim();
-                        String[] terms = line.split(" ");
-                        
-                        for(String term : terms)
-                        {
-                            if(query.equals(term))
-                            {
-                                tfidfs[i]++;
-                            }
-                            
-                            totalTerms++;
-                        }
-                        
-                        line = br.readLine();
-                    }
+                    break;
                 }
-                catch(IOException e)
+
+                Set<Path> matchingDocuments = invertedIndex.get(queryTerm);
+                matchingDocuments = matchingDocuments == null ? new HashSet<>() : matchingDocuments;
+
+                final List<Pair> pairs = calcTermScores(queryTerm, matchingDocuments, totalDocuments);
+
+                System.out.println("Identifying the " + DOCUMENTS_TO_SHOW + " document(s) most relevant to the query...");
+                System.out.println();
+
+                for(int i = 0; i < DOCUMENTS_TO_SHOW && i < pairs.size(); ++i)
                 {
-                    e.printStackTrace();
+                    System.out.println(pairs.get(i).document.getFileName() + " with a TF-IDF of " + pairs.get(i).tfidf);
                 }
-                
-                tfidfs[i] *= idf;
+
+                System.out.println();
+                System.out.println(Math.min(DOCUMENTS_TO_SHOW, pairs.size()) + " out of " + matchingDocuments.size() + " documents listed.");
             }
-            
-            final List<Pair> pairs = new ArrayList<>();
-            
-            for(int i = 0; i < documents.size(); i++)
-            {
-                pairs.add(new Pair(documents.get(i), tfidfs[i]));
-            }
-            
-            Collections.sort(pairs);
-            Collections.reverse(pairs);
-            
-            int i = 0;
-            
-            while(i < 5 && i < pairs.size())
-            {
-                System.out.println(pairs.get(i).file.getName() + " with a TF-IDF of " + pairs.get(i).termFrequency + "%");
-                i++;
-            }
-            
-            System.out.println();
-            System.out.println(Math.min(5, pairs.size()) + " item(s) listed out of " + documents.size() + " documents.");
+        }
+        catch(final IOException e)
+        {
+            System.out.println("Error: No such directory.");
         }
     }
-    
-    public static Map<String, List<File>> createInvertedIndex(
-        final String folderPath)
+
+    public static List<Pair> calcTermScores(final String queryTerm, final Set<Path> matchingDocuments, final long totalDocuments) throws IOException
     {
-        final File folder = new File(folderPath);
-        final File[] documents = folder.listFiles();
-        final Map<String, List<File>> invertedIndex = new HashMap<>();
-        
-        for(File document : documents)
+        final List<Pair> pairs = new ArrayList<>();
+        final double idf = matchingDocuments.size() > 0 ? Math.log(totalDocuments / matchingDocuments.size()) : 0;
+
+        for(Path document : matchingDocuments)
         {
-            try(BufferedReader br = new BufferedReader(new FileReader(document)))
+            int tf = 0;
+
+            try(final Scanner termScanner = new Scanner(document))
             {
-                String line = br.readLine();
-                
-                while(line != null)
+                termScanner.useDelimiter("[^A-Z|a-z]+");
+
+                while(termScanner.hasNext())
                 {
-                    line = line.toLowerCase().replaceAll("[^a-z]+", " ").trim();
-                    String[] terms = line.split("\\s");
-                    
-                    for(String term : terms)
+                    final String term = termScanner.next();
+
+                    if(term.equalsIgnoreCase(queryTerm))
                     {
-                        if(!invertedIndex.containsKey(term))
-                        {
-                            invertedIndex.put(term, new ArrayList<>());
-                        }
-                        
-                        if(!invertedIndex.get(term).contains(document))
-                        {
-                            invertedIndex.get(term).add(document);
-                        }
+                        ++tf;
                     }
-                    
-                    line = br.readLine();
                 }
             }
-            catch(IOException e)
-            {
-                e.printStackTrace();
-            }
+
+            pairs.add(new Pair(document, tf * idf));
         }
-        
+
+        Collections.sort(pairs);
+        Collections.reverse(pairs);
+
+        return pairs;
+    }
+
+    public static Map<String, Set<Path>> createInvertedIndex(final Path directory) throws IOException
+    {
+        final Map<String, Set<Path>> invertedIndex = new HashMap<>();
+
+        final Iterator<Path> documentIterator = Files.list(directory).iterator();
+
+        while(documentIterator.hasNext())
+        {
+            indexByDocument(documentIterator.next(), invertedIndex);
+        }
+
         return invertedIndex;
     }
-    
-    private static class Pair
-        implements Comparable<Pair>
+
+    private static void indexByDocument(final Path document, final Map<String, Set<Path>> invertedIndex) throws IOException
     {
-        private final File file;
-        private final double termFrequency;
-        
-        public Pair(
-            final File file,
-            final double termFrequency)
+        try(final Scanner termScanner = new Scanner(document))
         {
-            this.file = file;
-            this.termFrequency = termFrequency;
+            termScanner.useDelimiter("[^A-Z|a-z]+");
+
+            while(termScanner.hasNext())
+            {
+                final String term = termScanner.next().toLowerCase();
+
+                if(!invertedIndex.containsKey(term))
+                {
+                    invertedIndex.put(term, new HashSet<>());
+                }
+
+                invertedIndex.get(term).add(document);
+            }
         }
-        
-        public int compareTo(
-            final Pair o)
+    }
+
+    private static class Pair implements Comparable<Pair>
+    {
+        private final Path document;
+        private final double tfidf;
+
+        public Pair(final Path document, final double tfidf)
         {
-            if(termFrequency > o.termFrequency)
+            this.document = document;
+            this.tfidf = tfidf;
+        }
+
+        public int compareTo(final Pair o)
+        {
+            if(tfidf > o.tfidf)
             {
                 return 1;
             }
-            
-            if(termFrequency < o.termFrequency)
+
+            if(tfidf < o.tfidf)
             {
                 return -1;
             }
-            
+
             return 0;
         }
-        
+
         public String toString()
         {
-            return "[" + file.getName() + ", " + termFrequency + "]";
+            return "[" + document.getFileName() + ", " + tfidf + "]";
         }
     }
 }
